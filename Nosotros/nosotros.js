ab80ab6485c486452e1a991234afd1ed3f4d6062
Chala785 <juan_gallego82241@elpoli.edu.co>
2026-06-03 09:08:53 -0500

    first commit

---
+++ Nosotros/nosotros.js
@@ -0,0 +1,7 @@
+// MENU HAMBURGUESA
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
